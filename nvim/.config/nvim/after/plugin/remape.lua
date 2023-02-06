0f3ef4ce6cc389a156e950fe9f7e3b368c568b77
Krunal Akbari <krunalakbari233@gmail.com>
2023-02-06 13:53:23 +0530

    new changes i donk know where it hits

--- nvim/.config/nvim/after/plugin/remape.lua
+++
@@ -1,8 +0,0 @@
---for page in in system clipboard
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
-
---new project
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ./~/.dofiles/bin/.local/bin/tmux-sessionizer<cr>")
--- nvim/.config/nvim/lua/config/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.lsp")
-require("config.dap").setup() -- dap/init.lua
-require("config.treesitter").setup() -- treesitter/init.lua
-require("config.go").setup() --go/init.go
